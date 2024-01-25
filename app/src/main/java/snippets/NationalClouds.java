7867f0a550d4a37c0c280258bba4da74618e713c
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-01-25 11:36:40 -0800

    National Clouds

--- app/src/main/java/snippets/NationalClouds.java
+++ app/src/main/java/snippets/NationalClouds.java
@@ -9,13 +9,13 @@ import java.util.List;
 import com.azure.identity.AzureAuthorityHosts;
 import com.azure.identity.InteractiveBrowserCredential;
 import com.azure.identity.InteractiveBrowserCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.requests.GraphServiceClient;
+import com.microsoft.graph.core.authentication.AzureIdentityAuthenticationProvider;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 
 import okhttp3.Request;
 
 public class NationalClouds {
-    public static GraphServiceClient<Request> createClientForUSGov() throws Exception {
+    public static GraphServiceClient createClientForUSGov() throws Exception {
         // <NationalCloudSnippet>
         // Create the InteractiveBrowserCredential using details
         // from app registered in the Azure AD for US Government portal
@@ -25,22 +25,18 @@ public class NationalClouds {
             .authorityHost(AzureAuthorityHosts.AZURE_GOVERNMENT)
             .redirectUrl("YOUR_REDIRECT_URI").build();
 
-        final List<String> scopes = Arrays.asList("https://graph.microsoft.us/.default");
+        final String[] scopes = new String[] {"https://graph.microsoft.us/.default"};
 
         // Create the authentication provider
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
-
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // Set the service root to the
         // Microsoft Graph for US Government L4 endpoint
         // NOTE: The API version must be included in the URL
-        graphClient.setServiceRoot("https://graph.microsoft.us/v1.0");
+        graphClient.getRequestAdapter().setBaseUrl("https://graph.microsoft.us/v1.0");
         // </NationalCloudSnippet>
 
         return graphClient;
