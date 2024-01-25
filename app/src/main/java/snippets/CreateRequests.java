042338630aee95d288e0350c454c6541c4f74ef1
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-01-25 13:39:16 -0800

    Create Requests

--- app/src/main/java/snippets/CreateRequests.java
+++ app/src/main/java/snippets/CreateRequests.java
@@ -5,38 +5,28 @@ package snippets;
 
 import java.util.Objects;
 
-import com.microsoft.graph.models.Calendar;
-import com.microsoft.graph.models.GiphyRatingType;
-import com.microsoft.graph.models.Message;
-import com.microsoft.graph.models.Team;
-import com.microsoft.graph.models.TeamFunSettings;
-import com.microsoft.graph.models.User;
-import com.microsoft.graph.options.HeaderOption;
-import com.microsoft.graph.options.QueryOption;
-import com.microsoft.graph.requests.EventCollectionPage;
-import com.microsoft.graph.requests.GraphServiceClient;
-import com.microsoft.graph.requests.GroupCollectionPage;
-import com.microsoft.graph.requests.MessageCollectionPage;
+import com.microsoft.graph.models.*;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 
 import okhttp3.Request;
 
 public class CreateRequests {
-    public static void runSamples(GraphServiceClient<Request> graphClient) {
+    public static void runSamples(GraphServiceClient graphClient) {
         // Create a new message
         final Message tempMessage = new Message();
-        tempMessage.subject = "Temporary";
+        tempMessage.setSubject("Temporary");
 
         final Message createdMessage = Objects
-            .requireNonNull(graphClient.me().messages().buildRequest().post(tempMessage));
+            .requireNonNull(graphClient.me().messages().post(tempMessage));
 
-        final String messageId = createdMessage.id;
+        final String messageId = createdMessage.getId();
 
         // Get a team to update
-        final GroupCollectionPage teams = Objects
-            .requireNonNull(graphClient.groups().buildRequest()
-                .filter("resourceProvisioningOptions/Any(x:x eq 'Team')").get());
+        final GroupCollectionResponse teams = Objects.requireNonNull(graphClient.groups().get( requestConfiguration -> {
+            requestConfiguration.queryParameters.filter = "resourceProvisioningOptions/Any(x:x eq 'Team')";
+        }));
 
-        final String teamId = teams.getCurrentPage().get(0).id;
+        final String teamId = teams.getValue().get(0).getId();
 
         makeReadRequest(graphClient);
         makeSelectRequest(graphClient);
@@ -50,38 +40,41 @@ public class CreateRequests {
         makeQueryParametersRequest(graphClient);
     }
 
-    private static User makeReadRequest(GraphServiceClient<Request> graphClient) {
+    private static User makeReadRequest(GraphServiceClient graphClient) {
         // <ReadRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me
-        final User user = graphClient.me().buildRequest().get();
+        final User user = graphClient.me().get();
         // </ReadRequestSnippet>
 
         return user;
     }
 
-    private static User makeSelectRequest(GraphServiceClient<Request> graphClient) {
+    private static User makeSelectRequest(GraphServiceClient graphClient) {
         // <SelectRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me?$select=displayName,jobTitle
-        final User user = graphClient.me().buildRequest().select("displayName,jobTitle")
-            .get();
+        final User user = graphClient.me().get( requestConfiguration -> {
+            requestConfiguration.queryParameters.select = new String[] {"displayName", "jobTitle"};
+        });
         // </SelectRequestSnippet>
 
         return user;
     }
 
-    private static MessageCollectionPage makeListRequest(
-        GraphServiceClient<Request> graphClient) {
+    private static MessageCollectionResponse makeListRequest(
+        GraphServiceClient graphClient) {
         // <ListRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me/messages?
         // $select=subject,sender&$filter=subject eq 'Hello world'
-        final MessageCollectionPage messages = graphClient.me().messages().buildRequest()
-            .select("subject,sender").filter("subject eq 'Hello world'").get();
+        final MessageCollectionResponse messages = graphClient.me().messages().get( requestConfiguration -> {
+            requestConfiguration.queryParameters.select = new String[] {"subject", "sender"};
+            requestConfiguration.queryParameters.filter = "subject eq 'Hello world'";
+        });
         // </ListRequestSnippet>
 
         return messages;
     }
 
-    private static Message makeItemByIdRequest(GraphServiceClient<Request> graphClient,
+    private static Message makeItemByIdRequest(GraphServiceClient graphClient,
         String messageId) {
         if (null == messageId) {
             return null;
@@ -89,13 +82,13 @@ public class CreateRequests {
         // <ItemByIdRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me/messages/{message-id}
         // messageId is a string containing the id property of the message
-        final Message message = graphClient.me().messages(messageId).buildRequest().get();
+        final Message message = graphClient.me().messages().byMessageId(messageId).get();
         // </ItemByIdRequestSnippet>
 
         return message;
     }
 
-    private static Message makeExpandRequest(GraphServiceClient<Request> graphClient,
+    private static Message makeExpandRequest(GraphServiceClient graphClient,
         String messageId) {
         if (null == messageId) {
             return null;
@@ -104,14 +97,15 @@ public class CreateRequests {
         // GET
         // https://graph.microsoft.com/v1.0/me/messages/{message-id}?$expand=attachments
         // messageId is a string containing the id property of the message
-        final Message message = graphClient.me().messages(messageId).buildRequest()
-            .expand("attachments").get();
+        final Message message = graphClient.me().messages().byMessageId(messageId).get( requestConfiguration -> {
+            requestConfiguration.queryParameters.expand = new String[] {"attachments"};
+        });
         // </ExpandRequestSnippet>
 
         return message;
     }
 
-    private static void makeDeleteRequest(GraphServiceClient<Request> graphClient,
+    private static void makeDeleteRequest(GraphServiceClient graphClient,
         String messageId) {
         if (null == messageId) {
             return;
@@ -119,24 +113,23 @@ public class CreateRequests {
         // <DeleteRequestSnippet>
         // DELETE https://graph.microsoft.com/v1.0/me/messages/{message-id}
         // messageId is a string containing the id property of the message
-        graphClient.me().messages(messageId).buildRequest().delete();
+        graphClient.me().messages().byMessageId(messageId).delete();
         // </DeleteRequestSnippet>
     }
 
-    private static Calendar makeCreateRequest(GraphServiceClient<Request> graphClient) {
+    private static Calendar makeCreateRequest(GraphServiceClient graphClient) {
         // <CreateRequestSnippet>
         // POST https://graph.microsoft.com/v1.0/me/calendars
         final Calendar calendar = new Calendar();
-        calendar.name = "Volunteer";
+        calendar.setName("Volunteer");
 
-        final Calendar newCalendar = graphClient.me().calendars().buildRequest()
-            .post(calendar);
+        final Calendar newCalendar = graphClient.me().calendars().post(calendar);
         // </CreateRequestSnippet>
 
         return newCalendar;
     }
 
-    private static void makeUpdateRequest(GraphServiceClient<Request> graphClient,
+    private static void makeUpdateRequest(GraphServiceClient graphClient,
         String teamId) {
         if (null == teamId) {
             return;
@@ -145,38 +138,36 @@ public class CreateRequests {
         // PATCH https://graph.microsoft.com/v1.0/teams/{team-id}
         final Team team = new Team();
         final TeamFunSettings funSettings = new TeamFunSettings();
-        funSettings.allowGiphy = true;
-        funSettings.giphyContentRating = GiphyRatingType.STRICT;
-        team.funSettings = funSettings;
+        funSettings.setAllowGiphy(true);
+        funSettings.setGiphyContentRating(GiphyRatingType.Strict);
+        team.setFunSettings(funSettings);
 
         // teamId is a string containing the id property of the team
-        graphClient.teams(teamId).buildRequest().patch(team);
+        graphClient.teams().byTeamId(teamId).patch(team);
         // </UpdateRequestSnippet>
     }
 
-    private static EventCollectionPage makeHeadersRequest(
-        GraphServiceClient<Request> graphClient) {
+    private static EventCollectionResponse makeHeadersRequest(
+        GraphServiceClient graphClient) {
         // <HeadersRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me/events
-        final EventCollectionPage events = graphClient.me().events()
-            .buildRequest(
-                new HeaderOption("Prefer", "outlook.timezone=\"Pacific Standard Time\""))
-            .get();
+        final EventCollectionResponse events = graphClient.me().events().get( requestConfiguration -> {
+            requestConfiguration.headers.add("Prefer", "outlook.timezone=\"Pacific Standard Time\"");
+        });
         // </HeadersRequestSnippet>
 
         return events;
     }
 
-    private static EventCollectionPage makeQueryParametersRequest(
-        GraphServiceClient<Request> graphClient) {
+    private static EventCollectionResponse makeQueryParametersRequest(
+        GraphServiceClient graphClient) {
         // <QueryParametersRequestSnippet>
         // GET https://graph.microsoft.com/v1.0/me/calendarView?
         // startDateTime=2023-06-14T00:00:00Z&endDateTime=2023-06-15T00:00:00Z
-        final EventCollectionPage events = graphClient.me().events()
-            .buildRequest(
-                new QueryOption("startDateTime", "2023-06-14T00:00:00Z"),
-                new QueryOption("endDateTime", "2023-06-15T00:00:00Z")
-            ).get();
+        final EventCollectionResponse events = graphClient.me().calendarView().get( requestConfiguration -> {
+            requestConfiguration.queryParameters.startDateTime = "2023-06-14T00:00:00Z";
+            requestConfiguration.queryParameters.endDateTime = "2023-06-15T00:00:00Z";
+        });
         // </QueryParametersRequestSnippet>
 
         return events;
