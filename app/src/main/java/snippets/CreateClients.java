f5cc2fc22fde0d47d8ec98c58e7ad56d63a8b9d5
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-01-24 16:04:12 -0800

    Update Create Clients Snippets

--- app/src/main/java/snippets/CreateClients.java
+++ app/src/main/java/snippets/CreateClients.java
@@ -3,9 +3,6 @@
 
 package snippets;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.azure.identity.AuthorizationCodeCredential;
 import com.azure.identity.AuthorizationCodeCredentialBuilder;
 import com.azure.identity.ClientCertificateCredential;
@@ -20,13 +17,10 @@ import com.azure.identity.OnBehalfOfCredential;
 import com.azure.identity.OnBehalfOfCredentialBuilder;
 import com.azure.identity.UsernamePasswordCredential;
 import com.azure.identity.UsernamePasswordCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.requests.GraphServiceClient;
-
-import okhttp3.Request;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 
 public class CreateClients {
-    public static GraphServiceClient<Request> createWithAuthorizationCode()
+    public static GraphServiceClient createWithAuthorizationCode()
         throws Exception {
         // <AuthorizationCodeSnippet>
         final String clientId = "YOUR_CLIENT_ID";
@@ -34,7 +28,7 @@ public class CreateClients {
         final String clientSecret = "YOUR_CLIENT_SECRET";
         final String authorizationCode = "AUTH_CODE_FROM_REDIRECT";
         final String redirectUrl = "YOUR_REDIRECT_URI";
-        final List<String> scopes = Arrays.asList("User.Read");
+        final String[] scopes = new String[] { "User.Read" };
 
         final AuthorizationCodeCredential credential = new AuthorizationCodeCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).clientSecret(clientSecret)
@@ -43,17 +37,14 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </AuthorizationCodeSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithClientSecret() throws Exception {
+    public static GraphServiceClient createWithClientSecret() throws Exception {
         // <ClientSecretSnippet>
         final String clientId = "YOUR_CLIENT_ID";
         final String tenantId = "YOUR_TENANT_ID";
@@ -63,7 +54,7 @@ public class CreateClients {
         // /.default scope, and pre-configure your permissions on the
         // app registration in Azure. An administrator must grant consent
         // to those permissions beforehand.
-        final List<String> scopes = Arrays.asList("https://graph.microsoft.com/.default");
+        final String[] scopes = new String[] { "https://graph.microsoft.com/.default" };
 
         final ClientSecretCredential credential = new ClientSecretCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).clientSecret(clientSecret).build();
@@ -71,17 +62,14 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </ClientSecretSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithClientCertificate()
+    public static GraphServiceClient createWithClientCertificate()
         throws Exception {
         // <ClientCertificateSnippet>
         final String clientId = "YOUR_CLIENT_ID";
@@ -92,7 +80,7 @@ public class CreateClients {
         // /.default scope, and pre-configure your permissions on the
         // app registration in Azure. An administrator must grant consent
         // to those permissions beforehand.
-        final List<String> scopes = Arrays.asList("https://graph.microsoft.com/.default");
+        final String[] scopes = new String[] {"https://graph.microsoft.com/.default"};
 
         final ClientCertificateCredential credential = new ClientCertificateCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).pemCertificate(clientCertificatePath)
@@ -101,22 +89,19 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </ClientCertificateSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithOnBehalfOf() throws Exception {
+    public static GraphServiceClient createWithOnBehalfOf() throws Exception {
         // <OnBehalfOfSnippet>
         final String clientId = "YOUR_CLIENT_ID";
         final String tenantId = "YOUR_TENANT_ID"; // or "common" for multi-tenant apps
         final String clientSecret = "YOUR_CLIENT_SECRET";
-        final List<String> scopes = Arrays.asList("https://graph.microsoft.com/.default");
+        final String[] scopes = new String[] {"https://graph.microsoft.com/.default"};
 
         // This is the incoming token to exchange using on-behalf-of flow
         final String oboToken = "JWT_TOKEN_TO_EXCHANGE";
@@ -128,21 +113,18 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </OnBehalfOfSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithDeviceCode() throws Exception {
+    public static GraphServiceClient createWithDeviceCode() throws Exception {
         // <DeviceCodeSnippet>
         final String clientId = "YOUR_CLIENT_ID";
         final String tenantId = "YOUR_TENANT_ID"; // or "common" for multi-tenant apps
-        final List<String> scopes = Arrays.asList("User.Read");
+        final String[] scopes = new String[] {"User.Read"};
 
         final DeviceCodeCredential credential = new DeviceCodeCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).challengeConsumer(challenge -> {
@@ -153,22 +135,19 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </DeviceCodeSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithInteractive() throws Exception {
+    public static GraphServiceClient createWithInteractive() throws Exception {
         // <InteractiveSnippet>
         final String clientId = "YOUR_CLIENT_ID";
         final String tenantId = "YOUR_TENANT_ID"; // or "common" for multi-tenant apps
         final String redirectUrl = "YOUR_REDIRECT_URI";
-        final List<String> scopes = Arrays.asList("User.Read");
+        final String[] scopes = new String[] {"User.Read"};
 
         final InteractiveBrowserCredential credential = new InteractiveBrowserCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).redirectUrl(redirectUrl).build();
@@ -176,24 +155,21 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </InteractiveSnippet>
 
         return graphClient;
     }
 
-    public static GraphServiceClient<Request> createWithUserNamePassword()
+    public static GraphServiceClient createWithUserNamePassword()
         throws Exception {
         // <UserNamePasswordSnippet>
         final String clientId = "YOUR_CLIENT_ID";
         final String tenantId = "YOUR_TENANT_ID"; // or "common" for multi-tenant apps
         final String userName = "YOUR_USER_NAME";
         final String password = "YOUR_PASSWORD";
-        final List<String> scopes = Arrays.asList("User.Read");
+        final String[] scopes = new String[] {"User.Read"};
 
         final UsernamePasswordCredential credential = new UsernamePasswordCredentialBuilder()
             .clientId(clientId).tenantId(tenantId).username(userName).password(password)
@@ -202,11 +178,8 @@ public class CreateClients {
         if (null == scopes || null == credential) {
             throw new Exception("Unexpected error");
         }
-        final TokenCredentialAuthProvider authProvider = new TokenCredentialAuthProvider(
-            scopes, credential);
 
-        final GraphServiceClient<Request> graphClient = GraphServiceClient.builder()
-            .authenticationProvider(authProvider).buildClient();
+        final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
         // </UserNamePasswordSnippet>
 
         return graphClient;
